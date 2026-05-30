904af39723d9f9590bf094b1826f1f30eb502832
sspboy <sspboy@163.com>
2026-05-30 18:11:33 +0800

    '20260530'

--- src/components/AppMarket/Douyinshop/brand/brandlist.vue
+++ src/components/AppMarket/Douyinshop/brand/brandlist.vue
@@ -20,15 +20,64 @@
         placement="right"
 
     >
-        <div>
+        <!--
             <p>品牌列表</p>
-        </div>
+            更具当前选择的类目查询品牌列表
+            1、默认无品牌
+            2、无其它品牌列表 去添加
+            3、有品牌列表 选择品牌后 进行下一步操作
+            4、支持点击加载更多加载品牌列表
+            5、点击确认回填品牌id以及品牌名称
+            6、参考选择尺码模板组件方式设计
 
-        <div>下拉加载更多品牌</div>
+            <div>auth_required : 类目是否要求品牌有授权</div>
+            <div>授权的品牌列表</div>
+            <div>未授权的品牌列表</div>
+        -->
+
+
+        <a-form
+            layout="inline"
+            :model="formState"
+            @finish="handleFinish"
+            @finishFailed="handleFinishFailed"
+        >
+                <a-form-item label="品牌id">
+                    <a-input v-model:value="props.data.brand_id" placeholder="请输入品牌id" />
+                </a-form-item> 
+
+                <a-form-item label="品牌名称">
+                    <a-input v-model:value="props.data.brand_name" placeholder="请输入品牌名称" />
+                </a-form-item>
+
+                <a-form-item>
+                    <a-button type="primary" html-type="submit">查询品牌</a-button>
+                </a-form-item>
+        </a-form>
+        
+        <!-- 商品分类id未选择 -->
+        <a-radio-group v-model:value="BrandFun.radio_value" style="width: 100%;margin-top: 20px;">
+        <a-list :grid="{ gutter: [0,10], column: 1 }" :data-source="BrandFun.list" >
+            <template #renderItem="{ item }">
+                <a-list-item style="margin: 0;padding: 0;">
+                    <a-card>
+                        <a-radio :value="item.brand_id" class="font_size_12">
+                            <span> 
+                                品牌名称: {{item.name_cn}} 品牌id: {{item.brand_id}}
+                            </span>
+                        </a-radio>
+                    </a-card>
+                </a-list-item>
+            </template>
+        
+        </a-list>
+        </a-radio-group>
+        <!-- 商品分类id已选择 -->
+
 
         <template #footer>
             <a-space align="end" style="height: 100%;">
-                <a-button type="primary" @click="console.log('确认')">确认</a-button>
+                <a-button type="primary" @click="onSubmit">确认</a-button>
                 <a-button @click="onClose">取消</a-button>
             </a-space>
         </template>
@@ -36,10 +85,11 @@
 </template>
 
 <script>
-import { defineComponent,ref } from 'vue';
+import { defineComponent,ref,reactive } from 'vue';
 // 网络请求工具引用
 import * as TOOL from '@/assets/JS_Model/tool';
 import * as utils from '@/assets/JS_Model/public_model';
+import { t } from '@wangeditor/editor';
 export default defineComponent({
 
     name: "brandlist",  // 运费详情组件
@@ -59,40 +109,81 @@ export default defineComponent({
 
         const tool = new TOOL.TOOL()            // 工具方法
         const API = new utils.A_Patch()         // 请求接口地址合集
-
-        console.log(props.data) // 主模板
         
-        // 默认品牌列表查询
-        const load_get_brand=(c_id)=>{
+        // category_id 必填 - 按类目id推荐品牌
+        var category_id = props.data.category_id; // 当前选择的类目id
+        // 查询表单
+        const formState = reactive({
+            brand_id: '', // 品牌id
+            brand_name: '' // 品牌名称
+        });
 
-            tool.Http_.post(API.AppSrtoreAPI.dou_product.brand, {
-                "category_id":c_id
-            }).then((res)=>{
-                console.log(res)
-                // 成功
-                // 失败
-            })
+        const handleFinish = values => {
+            console.log(values, formState);
+        };
+
+        const handleFinishFailed = errors => {
+            console.log(errors);
+        };
+
+        console.log(category_id) // 主模板
+
+        const BrandFun=reactive({
+
+            // 596120136 默认品牌id-==无品牌
+            // 品牌列表
+            list:[
+                {
+                    brand_id:"596120136",
+                    name_cn:"无品牌",
+                    name_en:"无品牌"
+                },
+                {
+                    brand_id:"59612013611",
+                    name_cn:"无品牌",
+                    name_en:"无品牌"
+                }
+            ], 
+            brand_list_open:false, // 品牌列表抽屉开关
+            radio_value:undefined, // 品牌列表单选框选中值
+            selectcondition:{
+                    category_id:category_id, // 当前选择的类目id
+                    query:'', // 品牌前缀（中文或者英文），适用于不需要品牌资质的场景，根据前缀搜索品牌
+                    brand_ids:[] // 品牌ids，传了品牌ids默认直接拿品牌id搜索并返回结果(放在brand_list里)，不会使用其他入参
+            }, // 查询条件
+
+            // 默认品牌列表查询
+            load_get_brand:(data)=>{
+                tool.Http_.post(API.AppSrtoreAPI.dou_product.brand, data).then((res)=>{
+                    console.log(res)
+                })
+            }
+        })
+
+        
+
+
+        // 取消
+        const onClose = () =>{
+
+            props.data.brand_list_open = !props.data.brand_list_open;
 
         }
+        // 确认提交
+        const onSubmit = () =>{
+            console.log(BrandFun.radio_value)
+        }
 
-        // load_get_brand()
-
-        // category_id 必填 - 按类目id推荐品牌
-
-        // query 按关键词查询品牌品牌前缀
-
-        // brand_ids按品牌id查询
-      // 取消
-      const onClose = () =>{
-
-         props.data.brand_list_open = !props.data.brand_list_open;
-
-      }
     
         return{
             props,
             visible:ref(false),
-            onClose
+            BrandFun,
+            onClose,
+            onSubmit,
+            formState,
+            handleFinish,
+            handleFinishFailed
         }
     }
     })
